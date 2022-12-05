c43fbe122a7fba0ac57fbe2eeba1fad853e14dca
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-06 01:56:03 +0300

    feat: add solution read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'fs';
+
+import { getPathFile } from '../constants/getPathFile.js';
+
 const read = async () => {
-    // Write your code here 
+  const path = getPathFile(import.meta.url, '/files/fileToRead.txt');
+  const readStream = createReadStream(path, 'utf-8');
+  readStream.on('data', (data) => process.stdout.write(data));
+  readStream.on('error', (error) => console.log('Error', error.message));
 };
 
 await read();
