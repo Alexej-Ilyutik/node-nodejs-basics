9e1f438b26f6be2729887007a3178a399377b5ef
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-06 01:41:54 +0300

    feat: add solution calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
+import { getPathFile } from '../constants/getPathFile.js';
+import { errorMessage } from '../constants/errorMessage.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const path = getPathFile(
+    import.meta.url,
+    '/files/fileToCalculateHashFor.txt'
+  );
+  try {
+    const currentFile = await readFile(path);
+    const hash = createHash('SHA256').update(currentFile).digest('hex');
+    console.log(`Hash: ${hash}`);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await calculateHash();
