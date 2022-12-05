a89dea1e3deebc759161b8f0bbfcd6ec554fb805
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-06 02:08:02 +0300

    feat: add solution write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'fs';
+
+import { getPathFile } from '../constants/getPathFile.js';
+import { successMessage } from '../constants/successMessage.js';
+
 const write = async () => {
-    // Write your code here 
+  const path = getPathFile(import.meta.url, '/files/fileToWrite.txt');
+  const writeStream = createWriteStream(path);
+  process.stdin.on('data', (data) => {
+    writeStream.write(data.toString());
+    console.log(`Вы ввели: ${data} \n`);
+    console.log(
+      'Введите следующий текст. Либо нажмите ctrl+C, чтобы выйти.'
+    );
+  });
 };
 
 await write();
