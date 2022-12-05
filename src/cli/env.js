9524286a6b0ec9111a92f5b4ce6f1b018e8fe640
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-06 00:14:08 +0300

    feat: add solution parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const arrOfval = [];
+  Object.entries(process.env).forEach(([key, val]) => {
+    if (key.startsWith('RSS_')) {
+      arrOfval.push(`${key} = ${val}`);
+    }
+  });
+  console.log(arrOfval.join('; '));
 };
 
 parseEnv();
