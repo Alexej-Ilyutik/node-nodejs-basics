a59eb9fc57becc69fbf44edc79868f60e1e745af
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-06 02:24:55 +0300

    feat: add solution transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { pipeline, Transform } from 'stream';
+import { EOL } from 'os';
+
+import { errorMessage } from '../constants/errorMessage.js';
+
 const transform = async () => {
-    // Write your code here 
+  const revert = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(
+        null,
+        chunk.toString().replace(EOL, '').split('').reverse().join('') + EOL
+      );
+    },
+  });
+  pipeline(process.stdin, revert, process.stdout, (err)=>{
+    throw new Error(errorMessage);
+  });
+  console.log('Input text: \n');
 };
 
 await transform();
