b2df3afa6f0f13a4fd96202dbb3b61bb35dc5f09
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-05 21:37:07 +0300

    feat: add solution function delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { unlink } from 'fs/promises';
+
+import { getPathFile } from '../constants/getPathFile.js';
+import { errorMessage } from '../constants/errorMessage.js';
+import { successMessage } from '../constants/successMessage.js';
+
 const remove = async () => {
-    // Write your code here 
+  const deleteFile = getPathFile(import.meta.url, '/files/fileToRemove.txt');
+  try {
+    await unlink(deleteFile);
+    console.log(successMessage);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await remove();
