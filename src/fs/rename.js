c67139728f8502faa8c55d952281235c67919913
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-12-05 21:27:36 +0300

    feat: add solution function rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { rename as changeName} from 'fs/promises';
+
+import { getPathFile } from '../constants/getPathFile.js';
+import { errorMessage } from '../constants/errorMessage.js';
+import { successMessage } from '../constants/successMessage.js';
+
 const rename = async () => {
-    // Write your code here 
+  const wrongName = getPathFile(import.meta.url, '/files/wrongFilename.txt');
+  const newName = getPathFile(import.meta.url, '/files/properFilename.md');
+  try {
+    await changeName(wrongName, newName);
+    console.log(successMessage);
+  } catch (err) {
+    throw new Error(errorMessage);
+  }
 };
 
 await rename();
